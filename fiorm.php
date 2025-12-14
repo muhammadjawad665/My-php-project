ac69c11a0d7b0150b834e7a958cfef20f8b56549
Mjawad <mjd182665@gmail.com>
2025-12-14 11:05:18 +0500

    creating html form with using php

---
+++ fiorm.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>form</title>
+    <style>
+        
+    </style>
+</head>
+<body>
+<form action="formdata.php" method="post">
+    NAME : <input type="text" name="stname"><br><br>
+    FNAME : <input type="text" name="fname"><br><br>
+    CLASS : <input type="number" name="class"><br><br>
+    GR-NO : <input type="number" name="gr-no"><br><br>
+    TECHNOLOGY : <input type="text" name="tech"><br><br>
+    <input type="submit">
+    <input type="reset">
+
+
+</form>
+</body>
+</html>
---
+++ formdata.php
@@ -0,0 +1,16 @@
+<?php
+
+$name = $_POST['stname'];
+$fname = $_POST['fname'];
+$class = $_POST['class'];
+$grno = $_POST['gr-no'];
+$tech = $_POST['tech'];
+
+echo "<h1>My name is $name</h1>";
+echo "<h1>My father name is $fname </h1>";
+echo "<h1>My class is $class</h1>";
+echo "<h1>My grno is $grno</h1>";
+
+
+
+?>
